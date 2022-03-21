0e411b45d942237f83ce825d5617af670caf4f2f
DerpHead <alexraia4@gmail.com>
2022-03-21 18:31:19 -0400

    ...

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const { dictionary } = require('./dictionary')
+const fs = require('fs')
 
-let counter = 1 
 
-dictionary.forEach(element => {
-  console.log(element, '', counter)
-  counter++
-});
+const insert = "INSERT INTO Words (word)"
+const values = "VALUES ('"
+const end = "')"
+
+
+fs.writeFileSync('./derp.txt', 'word')
+
+
+
+// dictionary.map(word => {
+//   try {
+//     fs.writeFileSync('./derp.txt', word)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// })
